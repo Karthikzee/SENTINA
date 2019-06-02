502df1610c1de4cbd0e5ee4105ec96fa529ca747
Karthik <46178762+Karthikzee@users.noreply.github.com>
2019-06-02 12:07:26 +0530

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,16 @@
+CONSUMER_KEY = 'tnNtZgOuy6vF11UFzfiPPMVqu'
+CONSUMER_SECRET = 'Pa4rmi8U5gDROAUKacMq3Fi1Co1wXeawfHWzDRC7beeY8tUF4y'
+ACCESS_KEY = '1122411220215009280-qOnxXvvcldWWhPex6zAj2osa0KXzDe'
+ACCESS_SECRET = 'DNowAN5N7oygp5vFjMAVd52raiRsWDnFOS6C4U1ELhvwG'
+
+
+name_tag = """ 
+                   _______. _______ .__   __. .___________. __  .__   __.      ___      
+                  /       ||   ____||  | |  | |           ||  | |  | |  |     /   \\     
+                 |   (----`|  |__   |   ||  | `---|  |----`|  | |   ||  |    /  ^  \\    
+                  |   |    |   __|  |  . `  |     |  |     |  | |  . `  |   /  |_|  \\   
+              .----)   |   |  |____ |  ||   |     |  |     |  | |  ||   |  /  _____  \\  
+              |_______/    |_______||__| |__|     |__|     |__| |__| |__| /__|     |__\\ 
+                                
+                                SENTIMENT BASED TWEET REPLY BOT                                                  
+                                                                                  """
